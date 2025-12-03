8d7db352e5be6510fc68fe425584a82b544f761a
sukherbyviru5 <poetraarromadhon56@gmail.com>
2025-12-03 11:57:19 +0700

    update : scheduler

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,6 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+Schedule::command('inspire')->hourly();
+Schedule::command('queue:work --stop-when-empty --timeout=60 --tries=3')->everyMinute()->withoutOverlapping();
